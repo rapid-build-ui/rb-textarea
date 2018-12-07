18a568367058fe44c1695776fe1e638558c2d7a0
yokun <yevgeniyokun@gmail.com>
2018-12-06 21:56:21 -0600

    chore(init): add template

--- src/client/scripts/rb-textarea.js
+++ src/client/scripts/rb-textarea.js
@@ -2,15 +2,69 @@
  * RB-TEXTAREA
  ***********/
 import { props, html, RbBase } from '../../rb-base/scripts/rb-base.js';
+import FormControl from '../../form-control/scripts/form-control.js';
+import '../../rb-icon/scripts/rb-icon.js';
 import template from '../views/rb-textarea.html';
 
-export class RbTextarea extends RbBase() {
+export class RbTextarea extends FormControl(RbBase()) {
+	/* Lifecycle
+	 ************/
+	viewReady() { // :void
+		super.viewReady && super.viewReady();
+		this.rb.elms.focusElm = this.shadowRoot.querySelector('textarea');
+		this.rb.elms.formControl = this.rb.elms.focusElm
+	}
+
 	/* Properties
 	 *************/
-	static get props() {
+	static get props() { // :object
 		return {
-			kind: props.string
-		};
+			...super.props,
+			inline: props.boolean,
+			kind: props.string,
+			label: props.string,
+			placeholder: props.string,
+			right: props.boolean,
+			subtext: props.string,
+			type: props.string,
+			value: props.string,
+			_blurred: props.boolean,
+			_active: props.boolean,
+			_dirty: props.boolean
+		}
+	}
+
+	/* Observer
+	 ***********/
+	updating(prevProps) { // :void
+		if (prevProps.value === this.value) return;
+		this.rb.events.emit(this, 'value-changed', {
+			detail: { value: this.value }
+		});
+	}
+
+	/* Event Handlers
+	 *****************/
+	_onfocus(e) {
+		this._active = true;
+	}
+
+	async _oninput(e) { // TODO: add debouncing
+		const oldVal = this.value;
+		const newVal = e.target.value;
+		this.value = newVal;
+		if (!this._dirty && newVal !== oldVal)
+			return this._dirty = true;
+		if (!this._blurred) return;
+		await this.validate();
+	}
+
+	async _onblur(e) {
+		this._active = false;
+		if (!this._dirty) return;
+		this._blurred = true;
+		this.value = e.target.value;
+		await this.validate();
 	}
 
 	/* Template
