874c7e8ecbfa493fc0e5cfbe1a1e3dcf305638ff
Judd Younce <jyounce@yahoo.com>
2019-07-12 07:16:29 -0500

    chore(bump): v0.0.5

--- src/client/scripts/rb-textarea.js
+++ src/client/scripts/rb-textarea.js
@@ -13,7 +13,7 @@ export class RbTextarea extends FormControl(RbBase()) {
 	 ************/
 	constructor() {
 		super();
-		this.version = '0.0.4';
+		this.version = '0.0.5';
 		this.rb.formControl.isTextarea = true;
 	}
 	viewReady() { // :void
